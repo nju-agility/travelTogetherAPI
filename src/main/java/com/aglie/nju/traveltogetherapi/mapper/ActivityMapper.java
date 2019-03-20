e014685a2c28af978d719402ba0d963c70818fb5
liuyongxiang <295869813@qq.com>
2019-03-20 22:34:15 +0800

    create admin activity

--- src/main/java/com/aglie/nju/traveltogetherapi/mapper/ActivityMapper.java
+++ src/main/java/com/aglie/nju/traveltogetherapi/mapper/ActivityMapper.java
@@ -56,9 +56,12 @@ public interface ActivityMapper {
     /*******用户创建活动********/
     @Insert("insert into activities(status,owner,city,location,title,details,time_start,time_end,type,price)" +
             " values (0,#{owner},#{city},#{location},#{title},#{details},#{time_start},#{time_end},#{type},#{price})")
+   int addActivity(ActivityInfo activity);
 
-    public int addActivity(ActivityInfo activity);
-
+    /*******管理员创建活动********/
+    @Insert("insert into activities(status,owner,city,location,title,details,time_start,time_end,type,price)" +
+            " values (0,#{owner},#{city},#{location},#{title},#{details},#{time_start},#{time_end},'官方活动',#{price})")
+    int adminAddActivity(ActivityInfo activity);
 
     /*******查询用户正在创建的活动********/
     @Select("select * from activities where owner = #{owner} and time_start = #{time_start}")
